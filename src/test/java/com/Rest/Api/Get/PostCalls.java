3aa1b1fe2aedf2ef6f845f8f16021248ecc335a4
prajapatirajat94 <91628667+prajapatirajat94@users.noreply.github.com>
2022-12-15 13:43:57 -0500

    Update PostCalls.java

--- src/test/java/com/Rest/Api/Get/PostCalls.java
+++ src/test/java/com/Rest/Api/Get/PostCalls.java
@@ -7,6 +7,10 @@ import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 public class PostCalls {
 
+    
+ //below we are hitting api with Post calls but this is not good approach
+//but there are multiple option
+//1
 @Test
     public void Token_BDD_Post_Call_API_TEST(){
 
@@ -17,6 +21,19 @@ public class PostCalls {
             .then().log().all().assertThat().statusCode(200);
 
 }
+
+    //2 we are passing body as file
+
+    @Test
+    public void Token_BDD_Post_Call_API_FILE_TEST(){
+
+        RestAssured.baseURI="https://restful-booker.herokuapp.com";
+        given().log().all().contentType(ContentType.JSON)
+                .body(new File("C:\\Users\\s2743085\\OneDrive - The Bank of Nova Scotia\\Documents\\ProjectRunRajat\\src\\main\\java\\DataFiles\\credential.json"))
+                .when().log().all().post("/auth")
+                .then().log().all().assertThat().statusCode(200);
+
+    }
 
 
 
